ffd34cab22627610763504746994cb6d113964ce
MasterMunix <42143591+MasterMunix@users.noreply.github.com>
2021-05-27 23:05:39 -0400

    first-commit

---
+++ public/main.js
@@ -0,0 +1,10 @@
+//We select the iD of the button Update
+const update_button = document.getElementById('update');
+
+
+update_button.onclick = () => {
+    alert("klk");
+}
+// update_button.addEventListener('click', () => {
+//     alert("hola");
+// })
